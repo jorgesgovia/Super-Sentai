aa3a35bbd6ecbc72d9e73cd2c07ed653b0456ec1
Jorge Segovia <jorgesegovia@MacBook-Air-de-Jorge.local>
2026-08-12 13:00:04 -0600

    metadata fija Flashman

--- src/catalog.js
+++ src/catalog.js
@@ -9,33 +9,8 @@ export async function getCatalog() {
       const metadata = await buildMetadata(series);
 
       metas.push({
-        id: metadata.id,
-        type: "series",
-        name: metadata.name,
-        poster: metadata.poster,
-        background: metadata.background,
-        description: metadata.description,
-        releaseInfo: metadata.releaseInfo,
-        imdbRating: metadata.imdbRating,
-        tmdbRating: metadata.tmdbRating,
-
-        imdb_id: metadata.imdb_id,
-        imdbId: metadata.imdbId,
-
-        tmdb_id: metadata.tmdb_id,
-        tmdbId: metadata.tmdbId,
-        moviedb_id: metadata.moviedb_id,
-
-        tvdb_id: metadata.tvdb_id,
-        tvdbId: metadata.tvdbId,
-
-        trakt_id: metadata.trakt_id,
-        traktId: metadata.traktId,
-
-        mdblist_id: metadata.mdblist_id,
-        mdblistId: metadata.mdblistId,
-
-        ids: metadata.ids
+        ...metadata,
+        type: "series"
       });
     } catch (error) {
       console.error(
