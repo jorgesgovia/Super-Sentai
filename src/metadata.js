2268ab04673bb3bbea262ac68c15df22107be490
Jorge Segovia <jorgesegovia@MacBook-Air-de-Jorge.local>
2026-08-14 23:16:40 -0600

    experiment: remove network and production metadata

--- src/metadata.js
+++ src/metadata.js
@@ -2,29 +2,11 @@ export async function getMetadata() {
   return {
     id: "70787",
     type: "series",
-
     name: "Choushinsei Flashman",
-
     genres: [
       "Action",
       "Adventure",
       "Science Fiction"
-    ],
-
-    tmdb_id: "70787",
-
-    networks: [
-      {
-        id: 103,
-        name: "TV Asahi"
-      }
-    ],
-
-    productionCompanies: [
-      {
-        id: 5822,
-        name: "Toei Company"
-      }
     ]
   };
 }
