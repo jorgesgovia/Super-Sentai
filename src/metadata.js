b069f82c95b7941baa86e0d78ce49d2fb1f1ad23
Jorge Segovia <jorgesegovia@MacBook-Air-de-Jorge.local>
2026-08-14 23:02:21 -0600

    experiment: safe network production metadata

--- src/metadata.js
+++ src/metadata.js
@@ -3,90 +3,19 @@ export async function getMetadata() {
     id: "70787",
     type: "series",
 
-    /*
-     * IDENTIDAD
-     */
     name: "Choushinsei Flashman",
 
-    /*
-     * GENEROS
-     */
     genres: [
       "Action",
       "Adventure",
       "Science Fiction"
     ],
 
-    /*
-     * ========================================================
-     * ORIGINAL NETWORK
-     * ========================================================
-     */
-
     network: "TV Asahi",
-
-    originalNetwork: "TV Asahi",
-
-    original_network: "TV Asahi",
-
-    networks: [
-      {
-        name: "TV Asahi"
-      }
-    ],
-
-    originalNetworks: [
-      {
-        name: "TV Asahi"
-      }
-    ],
-
-    original_networks: [
-      {
-        name: "TV Asahi"
-      }
-    ],
-
-    /*
-     * ========================================================
-     * PRODUCTION COMPANY
-     * ========================================================
-     */
-
-    productionCompany: "Toei Company",
-
-    production_company: "Toei Company",
 
     productionCompanies: [
       {
         name: "Toei Company"
-      }
-    ],
-
-    production_companies: [
-      {
-        name: "Toei Company"
-      }
-    ],
-
-    /*
-     * Algunas estructuras utilizadas por diferentes
-     * sistemas de metadata.
-     */
-
-    production: {
-      company: "Toei Company",
-      name: "Toei Company"
-    },
-
-    productionCompanyList: [
-      "Toei Company"
-    ],
-
-    companies: [
-      {
-        name: "Toei Company",
-        type: "production"
       }
     ]
   };
